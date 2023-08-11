888f89ee6ba562e448b556ad60dcbb6073c54402
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-08-11 22:48:10 +0400

    Fix bug

--- condition.js
+++ condition.js
@@ -1,17 +1,17 @@
 var scoops = 10;
 while (scoops >= 0) {
     if (scoops == 3) {
-        alert("Ice cream is running low!");
+        console.log("Ice cream is running low!");
     } else if (scoops > 9) {
-        alert("Eat faster the ice cream is going to melt!");
+        console.log("Eat faster the ice cream is going to melt!");
     } else if (scoops == 2) {
-        alert("Going once!");
+        console.log("Going once!");
     } else if (scoops == 1) {
-        alert("Going twice!");
+        console.log("Going twice!");
     } else if(scoops == 0) {
-        alert("Gone!");
+        console.log("Gone!");
     } else {
-        alert("Still more ice cream left, come and get more obese! ");
+        console.log("Still more ice cream left, come and get more obese! ");
     }
     scoops -=1;
 }
@@ -22,17 +22,17 @@ var word1 = "a";
 var word2 = "nam";
 var word3 = "nal p";
 var word4 = "lan a c";
-var word5 = "a man a"; 
+var word5 = "a man a p"; 
 var phrase = "";
 for (let i = 0; i<4; i++) {
     if (i == 0) {
         phrase = word5;
     } else if (i == 1) {
-        phrase = phrase + word4;
+        phrase = phrase + word4; //a man a plan a c
     } else if (i == 2) {
-        phrase = phrase + word1 + word3;
-    } else if (i == 4) {
-        phrase = phrase + word4 + word2 + word1 
+        phrase = phrase + word1 + word3; //a man a plan a canal p
+    } else if (i == 3) {
+        phrase = phrase + word1 + word2 + word1;  //a man a plan a canal p
     }
 }
 alert(phrase);
