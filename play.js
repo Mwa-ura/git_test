064edd585d213be22a7b1b3c33705b870b3d08bb
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-08-15 22:36:51 +0400

    Add JS file for playlist html

---
+++ play.js
@@ -0,0 +1,14 @@
+window.onload = init;
+
+function init() {
+    var song0 = document.getElementById("song0");
+    var song1 = document.getElementById("song1");
+    var song2 = document.getElementById("song2");
+    var song3 = document.getElementById("song3");
+    var song4 = document.getElementById("song4");
+    song0.innerHTML = "Blue Suede, by Elvis Pagely";
+    song1.innerHTML = "Great Object on Fire, by Jerry JSON Jerry";
+    song2.innerHTML = "I code the line, JavaScript Lucas";
+    song3.innerHTML = "Oceans, by Hillsong United";
+    song4.innerHTML = "Gospel Mix, by Disk Joker Wendo";
+}
