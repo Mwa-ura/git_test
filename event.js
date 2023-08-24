14cae9fa8c4b1912ac2fe4cbd681eb55ba447307
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-08-24 19:13:32 +0400

    Add new song in DOM and display

--- event.js
+++ event.js
@@ -6,5 +6,18 @@ function init() {
 function handleButtonClick() {
     var textInput = document.getElementById("songInputText");
     var songName = textInput.value;
-    alert("adding "+ songName);
+    // Check whether the user have entered a text
+    if (songName == "") {
+        alert("You didn't add a song");
+    } else { 
+        alert("adding "+ songName);
+    }
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist");
+    if (li == "") {
+        return null;
+    } else { 
+        ul.appendChild(li);
+    }
 }
